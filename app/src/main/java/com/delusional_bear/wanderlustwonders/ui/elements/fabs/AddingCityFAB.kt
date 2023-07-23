1315c1621667244a50b197a593bf5e59ff0bb848
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-23 20:48:35 +0300

    Plain FAB was replaced with Large FAB

--- app/src/main/java/com/delusional_bear/wanderlustwonders/ui/elements/fabs/AddingCityFAB.kt
+++ app/src/main/java/com/delusional_bear/wanderlustwonders/ui/elements/fabs/AddingCityFAB.kt
@@ -1,26 +1,24 @@
 package com.delusional_bear.wanderlustwonders.ui.elements.fabs
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.delusional_bear.wanderlustwonders.R
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AddingCityFAB(modifier: Modifier = Modifier, onFABClick: () -> Unit) {
-    FloatingActionButton(
+    LargeFloatingActionButton(
         onClick = onFABClick,
         modifier = modifier,
     ) {
         Icon(
             imageVector = Icons.Filled.Add,
             contentDescription = null,
+            modifier = Modifier.size(36.dp),
         )
     }
 }
