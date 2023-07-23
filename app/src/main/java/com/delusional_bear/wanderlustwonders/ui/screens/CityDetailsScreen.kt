8dd09de94e587828cd522c494985bc72e3c10fe9
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-23 20:52:27 +0300

    Removed unnecessary ColorMatrix object. Added context. Added a row that holds some sort of functionality

--- app/src/main/java/com/delusional_bear/wanderlustwonders/ui/screens/CityDetailsScreen.kt
+++ app/src/main/java/com/delusional_bear/wanderlustwonders/ui/screens/CityDetailsScreen.kt
@@ -1,41 +1,37 @@
 package com.delusional_bear.wanderlustwonders.ui.screens
 
+import android.content.Intent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RichTooltipBox
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
@@ -44,6 +40,7 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.delusional_bear.wanderlustwonders.R
 import com.delusional_bear.wanderlustwonders.data.DataSource
+import com.delusional_bear.wanderlustwonders.ui.elements.buttons.CityDetailsIconButton
 
 enum class ImageState(val height: Dp) {
     Normal(330.dp),
@@ -56,8 +53,10 @@ fun CityDetailsScreen(modifier: Modifier = Modifier, dataSource: DataSource, cit
     var imageState by remember { mutableStateOf(ImageState.Normal) }
     val transition = updateTransition(targetState = imageState, label = null)
     val animateImageSize by transition.animateDp(label = "") { it.height }
+    val context = LocalContext.current
 
     val city = dataSource.getCityById(cityId)
+
     val bottomSheetState = rememberBottomSheetScaffoldState(
         bottomSheetState = SheetState(
             initialValue = SheetValue.Hidden,
@@ -66,7 +65,7 @@ fun CityDetailsScreen(modifier: Modifier = Modifier, dataSource: DataSource, cit
     )
 
     BottomSheetScaffold(
-        sheetContent = {  },
+        sheetContent = { },
         sheetPeekHeight = 210.dp,
         scaffoldState = bottomSheetState,
         modifier = modifier,
@@ -80,8 +79,6 @@ fun CityDetailsScreen(modifier: Modifier = Modifier, dataSource: DataSource, cit
                 .fillMaxSize()
                 .animateContentSize(),
         ) {
-            val matrix = ColorMatrix()
-            matrix.setToSaturation(0.6F)
             AsyncImage(
                 model = city?.imageURL,
                 contentDescription = city?.name,
@@ -90,49 +87,43 @@ fun CityDetailsScreen(modifier: Modifier = Modifier, dataSource: DataSource, cit
                     .fillMaxWidth()
                     .height(animateImageSize)
                     .clip(RoundedCornerShape(bottomStart = 48.dp, bottomEnd = 48.dp)),
-                colorFilter = ColorFilter.colorMatrix(matrix),
             )
             Spacer(modifier = Modifier.height(4.dp))
-            RichTooltipBox(
-                text = {
-                    Text(
-                        text = stringResource(id = R.string.hide_image_desc),
-                        style = MaterialTheme.typography.bodyMedium,
-                    )
-                },
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.hide_image),
-                        style = MaterialTheme.typography.titleSmall,
-                    )
-                },
-                action = {
-                    TextButton(onClick = { }) {
-                        Text(
-                            text = stringResource(id = R.string.learn_more),
-                            style = MaterialTheme.typography.titleSmall
-                        )
-                    }
-                },
-                modifier = Modifier
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
             ) {
-                IconButton(
-                    onClick = {
-                        imageState = if (imageState == ImageState.Normal) {
-                            ImageState.Hidden
-                        } else {
-                            ImageState.Normal
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentSize(align = Alignment.TopEnd)
-                        .tooltipAnchor(),
+                CityDetailsIconButton(
+                    tooltipText = R.string.hide_image_desc,
+                    tooltipTitle = R.string.hide_image,
+                    toolTipButtonText = R.string.learn_more,
+                    buttonIcon = R.drawable.hide_image,
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.hide_image),
-                        contentDescription = null,
+                    imageState = if (imageState == ImageState.Normal)
+                        ImageState.Hidden
+                    else
+                        ImageState.Normal
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                CityDetailsIconButton(
+                    tooltipText = R.string.share_desc,
+                    tooltipTitle = R.string.share,
+                    toolTipButtonText = R.string.learn_more,
+                    buttonIcon = R.drawable.share_icon,
+                ) {
+                    val shareMessage = context.resources.getString(
+                        R.string.share_message,
+                        city?.name,
+                        city?.country,
+                        city?.state ?: "",
+                        city?.description?.let { desc -> context.resources.getString(desc) }
                     )
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        putExtra(Intent.EXTRA_TEXT, shareMessage)
+                        type = "text/plain"
+                    }
+                    val shareIntent = Intent.createChooser(intent, null)
+                    context.startActivity(shareIntent)
                 }
             }
             Column(
@@ -140,7 +131,6 @@ fun CityDetailsScreen(modifier: Modifier = Modifier, dataSource: DataSource, cit
                     .verticalScroll(rememberScrollState())
                     .padding(16.dp)
             ) {
-
                 Text(
                     text = stringResource(id = city?.description ?: R.string.no_city_found),
                     style = MaterialTheme.typography.bodyMedium,
