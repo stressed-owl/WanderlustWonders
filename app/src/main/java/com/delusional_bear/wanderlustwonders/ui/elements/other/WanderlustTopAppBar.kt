795abfb317687130225619dd9a4032818e694758
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-24 14:56:28 +0300

    Added drop-down menu that allows users to sort the list of cities either by city or by country

--- app/src/main/java/com/delusional_bear/wanderlustwonders/ui/elements/other/WanderlustTopAppBar.kt
+++ app/src/main/java/com/delusional_bear/wanderlustwonders/ui/elements/other/WanderlustTopAppBar.kt
@@ -1,17 +1,19 @@
 package com.delusional_bear.wanderlustwonders.ui.elements.other
 
+import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.PlainTooltipBox
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.delusional_bear.wanderlustwonders.R
 
@@ -20,8 +22,12 @@ import com.delusional_bear.wanderlustwonders.R
 fun WanderlustTopAppBar(
     modifier: Modifier = Modifier,
     canNavigateBack: Boolean,
+    isDropDownMenuExpanded: Boolean,
+    onDropdownMenuDismiss: () -> Unit,
+    onOpenMenuClick: () -> Unit,
     onNavigateBackClick: () -> Unit,
     onSortByCityClick: () -> Unit,
+    onSortByCountryClick: () -> Unit,
 ) {
     TopAppBar(
         title = {
@@ -32,23 +38,25 @@ fun WanderlustTopAppBar(
         },
         actions = {
             if (!canNavigateBack) {
-                PlainTooltipBox(
-                    tooltip = {
-                        Text(
-                            text = stringResource(id = R.string.action_sort_cities_desc),
-                            style = MaterialTheme.typography.titleSmall,
-                        )
-                    },
+                IconButton(onClick = onOpenMenuClick) {
+                    Icon(imageVector = Icons.Filled.MoreVert, contentDescription = null)
+                }
+                DropdownMenu(
+                    expanded = isDropDownMenuExpanded,
+                    onDismissRequest = onDropdownMenuDismiss,
                 ) {
-                    IconButton(
+                    DropdownMenuItem(
+                        text = {
+                            DropDownMenuItemText(text = R.string.sort_by_city)
+                        },
                         onClick = onSortByCityClick,
-                        modifier = Modifier.tooltipAnchor()
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.sort_icon),
-                            contentDescription = null,
-                        )
-                    }
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            DropDownMenuItemText(text = R.string.sort_by_country)
+                        },
+                        onClick = onSortByCountryClick,
+                    )
                 }
             }
         },
@@ -63,5 +71,17 @@ fun WanderlustTopAppBar(
                 }
             }
         }
+    )
+}
+
+@Composable
+private fun DropDownMenuItemText(
+    @StringRes text: Int,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = stringResource(id = text),
+        modifier = modifier,
+        style = MaterialTheme.typography.titleSmall,
     )
 }
