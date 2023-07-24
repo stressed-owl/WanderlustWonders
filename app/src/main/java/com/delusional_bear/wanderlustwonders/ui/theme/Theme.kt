6d15cc96cee9564aa56c8f0b370103e03cdbf012
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-24 14:55:50 +0300

    DynamicColor was turned on

--- app/src/main/java/com/delusional_bear/wanderlustwonders/ui/theme/Theme.kt
+++ app/src/main/java/com/delusional_bear/wanderlustwonders/ui/theme/Theme.kt
@@ -84,7 +84,7 @@ private val DarkColors = darkColorScheme(
 fun WanderlustWondersTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
